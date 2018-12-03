75b14e6f380c393823067881007a0524aaabc5e3
Andreas Sumerauer <ansum@soundbytes.de>
2018-12-03 11:16:23 +0100

    a dedicated file for the api key

---
+++ lib/api/api_key.dart
@@ -0,0 +1,6 @@
+const String myTmdbApiKey = 'put your api key here';
+
+// the api read access token is not required for this app to run.
+// I am including it anyway because it is delivered together with the key
+// and might be of use later on.
+const String myTmdbApiReadAccessToken ='put the api read acces token here';
