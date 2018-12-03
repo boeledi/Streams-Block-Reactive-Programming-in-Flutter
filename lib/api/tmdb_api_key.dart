9925c15890b4a301bc5784bd1a42faa41eccab6e
Andreas Sumerauer <ansum@soundbytes.de>
2018-12-03 11:46:21 +0100

    Update tmdb_api_key.dart

--- lib/api/tmdb_api_key.dart
+++ lib/api/tmdb_api_key.dart
@@ -1,3 +1,10 @@
+// If you a hosting a fork of the app repository on any public server 
+// you are advised to remove this file from the repository after 
+// adding the secret key.
+// to do this run:
+//     git rm --cached lib/api/tmdb_api_key.dart
+//     git commit -m 'removed secret api key from repository' -o lib/api/tmdb_api_key.dart
+
 const String tmdbApiKey = 'put your api key here';
 
 // the api read access token is not required for this app to run.
