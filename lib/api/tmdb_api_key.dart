862bc746f59b7d3f567937ec951c528d4a9f01b2
Andreas Sumerauer <ansum@soundbytes.de>
2018-12-03 11:49:45 +0100

    improved security recommandation

--- lib/api/tmdb_api_key.dart
+++ lib/api/tmdb_api_key.dart
@@ -1,6 +1,6 @@
 // If you a hosting a fork of the app repository on any public server 
-// you are advised to remove this file from the repository after 
-// adding the secret key.
+// you are advised to remove this file from the repository before 
+// adding the secret api key.
 // to do this run:
 //     git rm --cached lib/api/tmdb_api_key.dart
 //     git commit -m 'removed secret api key from repository' -o lib/api/tmdb_api_key.dart
